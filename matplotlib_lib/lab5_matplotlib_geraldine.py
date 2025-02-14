178088b701b7f8af035d0c1d7a1f5656045d3d5a
chicacode <gerahernan.al@gmail.com>
2025-02-13 20:18:50 -0800

    matplotlib subplotting

--- matplotlib_lib/lab5_matplotlib_geraldine.py
+++ matplotlib_lib/lab5_matplotlib_geraldine.py
@@ -1,30 +1,32 @@
-# Import matplotlib
-
+# Vancouver February 13, 2025,
+# MatplotLib
+# Instructor: Derrick
+# Student: Geraldine
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Exercise 1: Basic Line Plot
+# 1. Exercise 1: Basic Line Plot
+
 # Data
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
+x = [0, 1, 2, 3, 4, 5, 6]
+y = [0, 1, 4, 9, 16, 25, 36]
+
 # Plotting
 plt.plot(x, y)
 
 # Labels
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
 
 # Title
-plt.title('Simple Line Plot')
+plt.title('First Line Plot')
 
 # Show
 plt.show()
 
 # Exercise 2: Customizing Plots
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
-# Plotting
-plt.plot(x, y, color='green', linestyle='--', marker='o')
+
+plt.plot(x, y, color='green', linestyle='--', marker='o', markersize=8, markerfacecolor='white')
 
 # Labels
 plt.xlabel('X-axis')
@@ -41,59 +43,98 @@ plt.show()
 
 # Exercise 3: Bar Plot
 
-categories = ['A', 'B', 'C', 'D', 'E']
-values = [5, 7, 3, 8, 6]
+# Data
+categories = ['Apples', 'Bananas', 'Cherries', 'Dates', 'Elderberries']
+values = [10, 15, 7, 12, 5]
+
 # Plotting
 plt.bar(categories, values)
+
+# Labels
 plt.xlabel('Categories')
 plt.ylabel('Values')
+
+#Title
 plt.title('Bar Plot Example')
+
+#show
 plt.show()
 
 # Exercise 4: Histogram
-data = np.random.randn(1000)
+
+# Data
+data = np.random.normal(0, 1, 500)
 
 # Plotting
-plt.hist(data, bins=30, edgecolor='black')
+plt.hist(data, bins=25, edgecolor='red')
+
+# Labels
 plt.xlabel('Value')
 plt.ylabel('Frequency')
-plt.title('Histogram Example')
+
+#Title
+plt.title('Histogram Exercise')
+
+#show
 plt.show()
 
 # Exercise 5: Scatter Plot
+
 # Data
+
 x = np.random.rand(50)
 y = np.random.rand(50)
+
 # Plotting
 plt.scatter(x, y)
+
+# Labels
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Scatter Plot Example')
+
+# title
+plt.title('Scatter Plot Exercise')
+
+# Show
 plt.show()
 
 # Exercise 6: Subplots
-# Data
+
+# Dataset
+
+# Line Plot
 x = [1, 2, 3, 4, 5]
 y = [1, 4, 9, 16, 25]
+
+# Bar Plot
 categories = ['A', 'B', 'C', 'D', 'E']
 values = [5, 7, 3, 8, 6]
+
+# Histogram
 data = np.random.randn(1000)
+
+# Scatter Plot
 x_scatter = np.random.rand(50)
 y_scatter = np.random.rand(50)
 
 # Plotting
 fig, axs = plt.subplots(2, 2)
+
 # Line Plot
 axs[0, 0].plot(x, y)
 axs[0, 0].set_title('Line Plot')
+
 # Bar Plot
 axs[0, 1].bar(categories, values)
 axs[0, 1].set_title('Bar Plot')
+
 # Histogram
-axs[1, 0].hist(data, bins=30, edgecolor='black')
+axs[1, 0].hist(data, bins=35, edgecolor='red')
 axs[1, 0].set_title('Histogram')
+
 # Scatter Plot
 axs[1, 1].scatter(x_scatter, y_scatter)
 axs[1, 1].set_title('Scatter Plot')
+
 plt.tight_layout()
 plt.show()
