c766188ef09460958c9534a0955890cdda78b653
chicacode <gerahernan.al@gmail.com>
2025-02-24 18:24:03 -0800

    matplotlib box plotting and pandas

---
+++ lab6/lab_6_students_grades.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Lab 6
+# Geraldine Hernandez A
+data = pd.read_csv('student_grades.csv')
+
+#print(data)
+
+# 1. Basic DataFrame Operations:
+
+# 1.1 Load the dataset into a Pandas DataFrame.
+
+
+# 1.2 Display the first 5 rows of the DataFrame.
+# 1.3 Display the summary statistics of the DataFrame.
+# 1.4 Display the column names of the DataFrame.
+# 1.5 Count the number of male and female students.
+
+# 2. Filtering and Subsetting:
+
+# 3. Aggregation and Grouping:
+
+
+# 4. Data Visualization:
